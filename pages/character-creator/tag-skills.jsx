db09e607b465f3b737a1ff00a2fc80eca63c543a
Nicholas MacDonald <macdonaldn@ufl.edu>
2024-11-26 14:56:51 -0500

    hide portrait page

--- pages/character-creator/tag-skills.jsx
+++ pages/character-creator/tag-skills.jsx
@@ -411,7 +411,7 @@ export default function SkillTagging(props) {
         },
         body: JSON.stringify({charData}),
       });
-      if (res.status === 200) return window.location.href = "/character-creator/portrait";
+      if (res.status === 200) return window.location.href = "/";
       const { error: message } = await res.json();
       setError(message);
     } catch (err) {
